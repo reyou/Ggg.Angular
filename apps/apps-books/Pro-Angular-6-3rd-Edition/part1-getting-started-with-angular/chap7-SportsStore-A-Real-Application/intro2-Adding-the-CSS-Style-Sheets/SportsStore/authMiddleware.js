4aa5f33e0736d24ee24160d42067198fca301635
teoman shipahi <aytekoz@yahoo.com>
2019-09-05 13:58:30 -0400

    Updating the Root Module

--- apps/apps-books/Pro-Angular-6-3rd-Edition/part1-getting-started-with-angular/chap7-SportsStore-A-Real-Application/intro2-Adding-the-CSS-Style-Sheets/SportsStore/authMiddleware.js
+++ apps/apps-books/Pro-Angular-6-3rd-Edition/part1-getting-started-with-angular/chap7-SportsStore-A-Real-Application/intro2-Adding-the-CSS-Style-Sheets/SportsStore/authMiddleware.js
@@ -3,10 +3,8 @@ const APP_SECRET = "myappsecret";
 const USERNAME = "admin";
 const PASSWORD = "secret";
 module.exports = function (req, res, next) {
-    if ((req.url == "/api/login" || req.url == "/login")
-        && req.method == "POST") {
-        if (req.body != null && req.body.name == USERNAME
-            && req.body.password == PASSWORD) {
+    if ((req.url == "/api/login" || req.url == "/login") && req.method == "POST") {
+        if (req.body != null && req.body.name == USERNAME && req.body.password == PASSWORD) {
             let token = jwt.sign({ data: USERNAME, expiresIn: "1h" }, APP_SECRET);
             res.json({ success: true, token: token });
         } else {
@@ -14,11 +12,9 @@ module.exports = function (req, res, next) {
         }
         res.end();
         return;
-    } else if ((((req.url.startsWith("/api/products")
-        || req.url.startsWith("/products"))
-        || (req.url.startsWith("/api/categories")
-            || req.url.startsWith("/categories"))) && req.method != "GET")
-        || ((req.url.startsWith("/api/orders") || req.url.startsWith("/orders")) && req.method != "POST")) {
+    } else if ((((req.url.startsWith("/api/products") || req.url.startsWith("/products")) || (req.url.startsWith("/api/categories")
+        || req.url.startsWith("/categories"))) && req.method != "GET") || ((req.url.startsWith("/api/orders") || req.url.startsWith("/orders"))
+            && req.method != "POST")) {
         let token = req.headers["authorization"];
         if (token != null && token.startsWith("Bearer<")) {
             token = token.substring(7, token.length - 1);
