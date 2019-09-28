9ae8777041905f4bdd4ccbd6d8a795eb2fd94894
Teoman Shipahi <aytekoz@yahoo.com>
2019-09-27 19:13:42 -0500

    subjects

--- apps/apps-web/www.youtube.com/PhggNGsSQyg-Learn-RxJS-in-60-Minutes/intro10-subjects/rxjs-learn/src/code.ts
+++ apps/apps-web/www.youtube.com/PhggNGsSQyg-Learn-RxJS-in-60-Minutes/intro10-subjects/rxjs-learn/src/code.ts
@@ -10,6 +10,19 @@ subject.subscribe(
 
 subject.next(`The first thing has been sent.`);
 
+let observer2 = subject.subscribe(
+  data => addItem(`Observer 2: ${data}`),
+  err => addItem(err),
+  () => addItem(`Observer 2 Completed`)
+);
+
+subject.next(`The second thing has been sent.`);
+subject.next(`A third thing has been sent.`);
+
+observer2.unsubscribe();
+
+subject.next(`A final thing has been sent.`);
+
 function addItem(val: any) {
   let node = document.createElement("li");
   let textnode = document.createTextNode(val);
