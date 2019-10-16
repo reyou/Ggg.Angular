786b3211578bf2caaba1439bc4d7ba2d52c5740e
Teoman Shipahi <aytekoz@yahoo.com>
2019-10-15 22:45:26 -0500

    Using Data Bindings

---
+++ apps/apps-books/Pro-Angular-6-3rd-Edition/part2-angular-in-detail/chap11-creating-an-angular-project/intro2-Adding-the-Bootstrap-CSS-Framework/example/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+// import { AppComponent } from './app.component';
+import { ProductComponent } from "./component";
+
+@NgModule({
+  declarations: [ProductComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [ProductComponent]
+})
+export class AppModule {}
