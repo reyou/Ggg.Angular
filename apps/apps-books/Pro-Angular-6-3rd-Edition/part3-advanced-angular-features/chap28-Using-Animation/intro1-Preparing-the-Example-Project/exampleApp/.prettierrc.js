64f165dc067fde54eef05e61c786d5fb1a8f62b3
Teoman Shipahi <aytekoz@yahoo.com>
2020-01-15 21:36:46 -0600

    Using Animation

---
+++ apps/apps-books/Pro-Angular-6-3rd-Edition/part3-advanced-angular-features/chap28-Using-Animation/intro1-Preparing-the-Example-Project/exampleApp/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  'max-line-length': [true, { limit: 100 }],
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true
+};
---
+++ apps/apps-books/Pro-Angular-6-3rd-Edition/part3-advanced-angular-features/chap28-Using-Animation/intro1-Preparing-the-Example-Project/exampleApp/src/app/core/table.animations.ts
@@ -0,0 +1,9 @@
+import { trigger, style, state, transition, animate } from '@angular/animations';
+export const HighlightTrigger = trigger('rowHighlight', [
+  state('selected', style({ backgroundColor: 'lightgreen', fontSize: '20px' })),
+  state('notselected', style({ backgroundColor: 'lightsalmon', fontSize: '12px' })),
+  state('void', style({ opacity: 0 })),
+  transition('* => notselected', animate('200ms')),
+  transition('* => selected', animate('400ms')),
+  transition('void => *', animate('500ms'))
+]);
