11f92b700a8a4be3d978545bbe41b4b8794794c3
aytekin ozdemir <aytekin.ozdemir@blackbaud.me>
2020-01-23 14:09:09 -0600

    https://angular.io/start/data#enable-httpclient-for-app

--- apps/apps-docs/angular.io/start/data/intro1-managing-data/appGenerateComponent.ps1
+++ apps/apps-docs/angular.io/start/data/intro1-managing-data/appGenerateComponent.ps1
@@ -5,7 +5,7 @@ $currentDir = Get-ScriptDirectory
 Write-Output "`n`nCurrent Directory: `n$currentDir`n`n" 
 Set-Location $currentDir
 Set-Location .\stackblitz
-# ng generate component product-details/
-ng generate service cart/
+ng generate component cart/
+# ng generate service cart/
 Set-Location .\..
 
