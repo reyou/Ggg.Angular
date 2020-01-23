d86ac5ee03b8b5a876e29bbb7c2b3db5cce70da4
aytekin ozdemir <aytekin.ozdemir@blackbaud.me>
2020-01-23 10:30:31 -0600

    product details.

--- apps/apps-docs/angular.io/start/data/intro1-managing-data/appStart.ps1
+++ apps/apps-docs/angular.io/start/data/intro1-managing-data/appStart.ps1
@@ -5,6 +5,6 @@ $currentDir = Get-ScriptDirectory
 Write-Output "`n`nCurrent Directory: `n$currentDir`n`n" 
 Set-Location $currentDir
 Set-Location .\stackblitz
-ng generate component product-details/
+# npm ci
+ng serve --port 3000 --open
 Set-Location .\..
-
