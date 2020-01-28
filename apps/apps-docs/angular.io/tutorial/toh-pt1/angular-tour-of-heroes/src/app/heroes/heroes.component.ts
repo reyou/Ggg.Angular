d913827c0548625f354be8b5953d39d38e4c0d06
Teoman Shipahi <aytekoz@yahoo.com>
2020-01-28 00:49:46 -0600

    Routable HeroDetailComponent

--- apps/apps-docs/angular.io/tutorial/toh-pt1/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ apps/apps-docs/angular.io/tutorial/toh-pt1/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,22 +9,14 @@ import { HeroService } from "../hero.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
   }
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe({
-      next: heroes => {
-        this.heroes = heroes;
-      }
-    });
-  }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 }
