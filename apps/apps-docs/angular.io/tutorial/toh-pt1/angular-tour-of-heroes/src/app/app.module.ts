0e766f626506fc53bdedb58234129109daf8a192
Teoman Shipahi <aytekoz@yahoo.com>
2020-01-27 23:50:13 -0600

    Routing

--- apps/apps-docs/angular.io/tutorial/toh-pt1/angular-tour-of-heroes/src/app/app.module.ts
+++ apps/apps-docs/angular.io/tutorial/toh-pt1/angular-tour-of-heroes/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { FormsModule } from "@angular/forms";
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { MessagesComponent } from "./messages/messages.component";
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
