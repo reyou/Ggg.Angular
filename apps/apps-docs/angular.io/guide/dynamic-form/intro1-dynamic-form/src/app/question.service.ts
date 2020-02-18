14d27dc4e69dd3fe60a7ed751ae3a580dfb4344d
aytekin ozdemir <aytekin.ozdemir@blackbaud.me>
2020-02-17 23:59:10 -0600

    Observables

--- apps/apps-docs/angular.io/guide/dynamic-form/intro1-dynamic-form/src/app/question.service.ts
+++ apps/apps-docs/angular.io/guide/dynamic-form/intro1-dynamic-form/src/app/question.service.ts
@@ -1,43 +1,48 @@
-import { Injectable }       from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { DropdownQuestion } from './question-dropdown';
-import { QuestionBase }     from './question-base';
-import { TextboxQuestion }  from './question-textbox';
-import { of } from 'rxjs';
+import { DropdownQuestion } from "./question-dropdown";
+import { QuestionBase } from "./question-base";
+import { TextboxQuestion } from "./question-textbox";
+import { of } from "rxjs";
 
 @Injectable()
 export class QuestionService {
-
   // TODO: get from a remote source of question metadata
   getQuestions() {
-
     let questions: QuestionBase<string>[] = [
-
       new DropdownQuestion({
-        key: 'brave',
-        label: 'Bravery Rating',
+        key: "brave",
+        label: "Bravery Rating",
         options: [
-          {key: 'solid',  value: 'Solid'},
-          {key: 'great',  value: 'Great'},
-          {key: 'good',   value: 'Good'},
-          {key: 'unproven', value: 'Unproven'}
+          { key: "solid", value: "Solid" },
+          { key: "great", value: "Great" },
+          { key: "good", value: "Good" },
+          { key: "unproven", value: "Unproven" }
         ],
-        order: 3
+        order: 4
       }),
 
       new TextboxQuestion({
-        key: 'firstName',
-        label: 'First name',
-        value: 'Bombasto',
+        key: "firstName",
+        label: "First name",
+        value: "Bombasto",
         required: true,
         order: 1
       }),
 
       new TextboxQuestion({
-        key: 'emailAddress',
-        label: 'Email',
-        type: 'email',
+        key: "lastName",
+        label: "Last name",
+        value: "Gulu-gulu",
+        required: true,
         order: 2
+      }),
+
+      new TextboxQuestion({
+        key: "emailAddress",
+        label: "Email",
+        type: "email",
+        order: 3
       })
     ];
 
