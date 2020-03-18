b9e5a3c223c4ef6b8ef84782f8d8be5610762414
Teoman Shipahi <aytekoz@yahoo.com>
2020-03-18 00:25:08 -0500

    http client

---
+++ apps/apps-docs/angular.io/guide/http/intro1-httpclient/src/app/downloader/downloader.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+import { DownloaderService } from "./downloader.service";
+
+@Component({
+  selector: "app-downloader",
+  templateUrl: "./downloader.component.html",
+  providers: [DownloaderService]
+})
+export class DownloaderComponent {
+  contents: string;
+  constructor(private downloaderService: DownloaderService) {}
+
+  clear() {
+    this.contents = undefined;
+  }
+
+  download() {
+    this.downloaderService.getTextFile("assets/textfile.txt").subscribe({
+      next: results => {
+        this.contents = results;
+      }
+    });
+  }
+}
