307babdb0daa9efabdd8618d830c91db621162ad
Teoman Shipahi <aytekoz@yahoo.com>
2020-03-20 23:37:44 -0500

    hERO feature routing requirements

--- apps/apps-docs/angular.io/guide/router/intro2-milestone-1-getting-started/angular-router-sample/src/app/app.module.ts
+++ apps/apps-docs/angular.io/guide/router/intro2-milestone-1-getting-started/angular-router-sample/src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+import { CrisisListComponent } from "./crisis-list/crisis-list.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { HeroesModule } from "./heroes/heroes.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HeroesModule],
+  declarations: [AppComponent, CrisisListComponent, PageNotFoundComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
