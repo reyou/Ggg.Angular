8344e9fe4f8b45918c956cbe32c9aaf56f739165
aytekin ozdemir <aytekin.ozdemir@blackbaud.me>
2020-03-25 23:39:26 -0500

    DATE AFTER TICK

--- apps/apps-web/medium.com/angular-testing-async-stuff/intro1-fake-async/intro1/src/app/intro1.spec.ts
+++ apps/apps-web/medium.com/angular-testing-async-stuff/intro1-fake-async/intro1/src/app/intro1.spec.ts
@@ -13,21 +13,28 @@ describe("Testing async stuff in the fakedAsync zone", () => {
     tick(16 * 1000);
   }));
 
-  it("drains the zone task by task 2", fakeAsync(() => {
+  fit("drains the zone task by task 2", fakeAsync(() => {
+    console.log("INITIAL DATE", new Date().toUTCString());
     setTimeout(() => {
       let i = 0;
       const handle = setInterval(() => {
+        console.log("First Interval:", i);
         if (++i === 5) {
+          console.log("Clear First Interval:", i);
           clearInterval(handle);
         }
       }, 1000);
       let j = 0;
       const handle2 = setInterval(() => {
+        console.log("Second Interval:", j);
         if (++j === 3) {
+          console.log("Clear Second Interval:", j);
           clearInterval(handle2);
         }
       }, 1000);
     }, 10000);
+    console.log("DATE BEFORE TICK", new Date().toUTCString());
     tick(15000);
+    console.log("DATE AFTER TICK", new Date().toUTCString());
   }));
 });
