031770c3f32efff1b611dabd18ce9768a0db97f6
Teoman Shipahi <aytekoz@yahoo.com>
2020-04-04 23:07:51 -0500

    ConnectableObservable

--- apps/apps-docs/rxjs-dev.firebaseapp.com/guide/subject/intro1/src/index.ts
+++ apps/apps-docs/rxjs-dev.firebaseapp.com/guide/subject/intro1/src/index.ts
@@ -1,10 +1,12 @@
-import Intro1Subject from './intro1-subject';
-import Intro2SubjectIsAnObserver from './intro2-Subject-is-an-Observer';
+import Intro1Subject from "./intro1-subject";
+import Intro2SubjectIsAnObserver from "./intro2-Subject-is-an-Observer";
+import Intro3MulticastedObservables from "./intro3-Multicasted-Observables";
 
 export default class Index {
   constructor() {
     new Intro1Subject();
-    new Intro2SubjectIsAnObserver().run();
+    new Intro2SubjectIsAnObserver();
+    new Intro3MulticastedObservables().run();
   }
 }
 new Index();
--- apps/apps-docs/rxjs-dev.firebaseapp.com/guide/subject/intro1/src/intro3-Multicasted-Observables.ts
+++ apps/apps-docs/rxjs-dev.firebaseapp.com/guide/subject/intro1/src/intro3-Multicasted-Observables.ts
@@ -1,1 +1,29 @@
 // https://rxjs-dev.firebaseapp.com/guide/subject#Multicasted-Observables
+
+import { from, Observable, Subject, ConnectableObservable } from "rxjs";
+import { multicast } from "rxjs/operators";
+export default class Intro3MulticastedObservables {
+  public run() {
+    const source: Observable<number> = from([1, 2, 3]);
+    const subject = new Subject();
+    const multicasted = source.pipe(
+      multicast(subject)
+    ) as ConnectableObservable<number>;
+
+    // These are, under the hood, `subject.subscribe({...})`:
+    multicasted.subscribe({
+      next: (v) => {
+        console.log(`observerA: ${v}`);
+      },
+    });
+
+    multicasted.subscribe({
+      next: (v) => {
+        console.log(`observerB: ${v}`);
+      },
+    });
+
+    // This is, under the hood, `source.subscribe(subject)`:
+    multicasted.connect();
+  }
+}
