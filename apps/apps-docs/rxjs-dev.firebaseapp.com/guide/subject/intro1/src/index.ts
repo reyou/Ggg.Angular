d536649e188aaf251b6c7f2568e9d1af217c0840
Teoman Shipahi <aytekoz@yahoo.com>
2020-04-04 23:24:16 -0500

    Reference-counting

--- apps/apps-docs/rxjs-dev.firebaseapp.com/guide/subject/intro1/src/index.ts
+++ apps/apps-docs/rxjs-dev.firebaseapp.com/guide/subject/intro1/src/index.ts
@@ -1,12 +1,14 @@
 import Intro1Subject from "./intro1-subject";
 import Intro2SubjectIsAnObserver from "./intro2-Subject-is-an-Observer";
 import Intro3MulticastedObservables from "./intro3-Multicasted-Observables";
+import Intro4ReferenceCounting from "./intro4-Reference-counting";
 
 export default class Index {
   constructor() {
     new Intro1Subject();
     new Intro2SubjectIsAnObserver();
-    new Intro3MulticastedObservables().run();
+    new Intro3MulticastedObservables();
+    new Intro4ReferenceCounting().run();
   }
 }
 new Index();
---
+++ apps/apps-docs/rxjs-dev.firebaseapp.com/guide/subject/intro1/src/intro4-Reference-counting.ts
@@ -0,0 +1,54 @@
+// https://rxjs-dev.firebaseapp.com/guide/subject#Reference-counting
+
+import {
+  from,
+  Observable,
+  Subject,
+  ConnectableObservable,
+  interval,
+  Subscription,
+} from "rxjs";
+import { multicast } from "rxjs/operators";
+export default class Intro4ReferenceCounting {
+  public run() {
+    const source: Observable<number> = interval(500);
+    const subject = new Subject();
+    const multicasted: ConnectableObservable<number> = source.pipe(
+      multicast(subject)
+    ) as ConnectableObservable<number>;
+
+    let subscription1: Subscription,
+      subscription2: Subscription,
+      subscriptionConnect: Subscription;
+
+    subscription1 = multicasted.subscribe({
+      next: (v) => {
+        console.log(`observerA: ${v}`);
+      },
+    });
+
+    // We should call `connect()` here, because the first
+    // subscriber to `multicasted` is interested in consuming values
+    subscriptionConnect = multicasted.connect();
+
+    setTimeout(() => {
+      subscription2 = multicasted.subscribe({
+        next: (v) => {
+          console.log(`observerB: ${v}`);
+        },
+      });
+    }, 600);
+
+    setTimeout(() => {
+      subscription1.unsubscribe();
+    }, 1200);
+
+    // We should unsubscribe the shared Observable execution here,
+    // because `multicasted` would have no more subscribers after this
+    setTimeout(() => {
+      subscription2.unsubscribe();
+      // for the shared Observable
+      subscriptionConnect.unsubscribe();
+    }, 2000);
+  }
+}
