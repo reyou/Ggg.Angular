39e7dfc27a405ea51e0398845a40f48d81257dda
Teoman Shipahi <aytekoz@yahoo.com>
2020-04-08 22:59:25 -0500

    forChild

--- apps/apps-docs/angular.io/guide/router/intro2-milestone-1-getting-started/angular-router-sample/src/app/app-routing.module.ts
+++ apps/apps-docs/angular.io/guide/router/intro2-milestone-1-getting-started/angular-router-sample/src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { CrisisListComponent } from "./crisis-list/crisis-list.component";
-import { HeroListComponent } from "./heroes/hero-list/hero-list.component";
+// import { HeroListComponent }  from './hero-list/hero-list.component';  // <-- delete this line
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const appRoutes: Routes = [
   { path: "crisis-center", component: CrisisListComponent },
-  { path: "heroes", component: HeroListComponent },
+  // { path: 'heroes',     component: HeroListComponent }, // <-- delete this line
   { path: "", redirectTo: "/heroes", pathMatch: "full" },
-  { path: "**", component: PageNotFoundComponent }
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
@@ -17,8 +17,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
-    )
+    ),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- apps/apps-docs/angular.io/guide/router/intro2-milestone-1-getting-started/angular-router-sample/src/app/app.module.ts
+++ apps/apps-docs/angular.io/guide/router/intro2-milestone-1-getting-started/angular-router-sample/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { HeroesModule } from "./heroes/heroes.module";
 
 import { CrisisListComponent } from "./crisis-list/crisis-list.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { HeroesModule } from "./heroes/heroes.module";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HeroesModule],
+  imports: [BrowserModule, FormsModule, HeroesModule, AppRoutingModule],
   declarations: [AppComponent, CrisisListComponent, PageNotFoundComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
