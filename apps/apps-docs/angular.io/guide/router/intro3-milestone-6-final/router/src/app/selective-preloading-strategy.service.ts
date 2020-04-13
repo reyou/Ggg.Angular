1a7d7365d46c66a918930081ce4a3ea3d5298aef
Teoman Shipahi <aytekoz@yahoo.com>
2020-04-12 23:49:56 -0500

    git router final example, milestone 6

--- apps/apps-docs/angular.io/guide/router/intro3-milestone-6-final/router/src/app/selective-preloading-strategy.service.ts
+++ apps/apps-docs/angular.io/guide/router/intro3-milestone-6-final/router/src/app/selective-preloading-strategy.service.ts
@@ -1,20 +1,20 @@
-import { Injectable } from "@angular/core";
-import { PreloadingStrategy, Route } from "@angular/router";
-import { Observable, of } from "rxjs";
+import { Injectable } from '@angular/core';
+import { PreloadingStrategy, Route } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class SelectivePreloadingStrategyService implements PreloadingStrategy {
   preloadedModules: string[] = [];
 
   preload(route: Route, load: () => Observable<any>): Observable<any> {
-    if (route.data && route.data["preload"]) {
+    if (route.data && route.data['preload']) {
       // add the route path to the preloaded module array
       this.preloadedModules.push(route.path);
 
       // log the route path to the console
-      console.log("Preloaded: " + route.path);
+      console.log('Preloaded: ' + route.path);
 
       return load();
     } else {
