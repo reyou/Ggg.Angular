18aefb3fe2bec691c14ec518cee968f122a9ef0c
Teoman Shipahi <aytekoz@yahoo.com>
2020-05-17 01:06:50 -0500

    component-test-scenarios

--- apps/apps-docs/angular.io/guide/testing/intro3-configure-cli-for-ci-testing-in-chrome/example-test-app/src/app/app-routing.module.ts
+++ apps/apps-docs/angular.io/guide/testing/intro3-configure-cli-for-ci-testing-in-chrome/example-test-app/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LightswitchComponent } from "./lightswitch-component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "lightswitch",
+    component: LightswitchComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- apps/apps-docs/angular.io/guide/testing/intro3-configure-cli-for-ci-testing-in-chrome/example-test-app/src/app/app.module.ts
+++ apps/apps-docs/angular.io/guide/testing/intro3-configure-cli-for-ci-testing-in-chrome/example-test-app/src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LightswitchComponent } from "./lightswitch-component";
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LightswitchComponent, BannerComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
